a20b38063533ca6ef515a185aaa9da631562ff3b
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-28 22:21:36 +0000

    login/logout
    user will only see the comments if loged in

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from '../Header';
 import Article from '../Article';
+import User from '../User';
+import { useAuth0 } from '@auth0/auth0-react';
+
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <main className="App">
       <Header />
-      <Article />
+      <User />
+      {isAuthenticated ? <Article /> : ''}
     </main>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import css from './Header.module.css'
 
+
 const Header = () => {
   return (
     <div className={css.container}>
       <h1>WikiPigeon</h1>
+      
     </div>
   );
 };
